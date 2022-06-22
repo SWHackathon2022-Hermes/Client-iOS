bc12554c9869538381aad5d2b718de9570adb0c2
임주민 <limj1213@gmail.com>
2022-06-22 23:42:39 +0900

    [FEAT] 탭바 구현

--- Hermes-iOS/Hermes-iOS/Sources/View/ContentView.swift
+++ Hermes-iOS/Hermes-iOS/Sources/View/ContentView.swift
@@ -8,14 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  @State private var selection: Tab = .map
+  
+  enum Tab {
+    case map
+    case toilet
+    case charger
+    case lift
+    case report
+  }
+  
+  var body: some View {
+    TabView(selection: $selection) {
+      MapView()
+        .tabItem {
+          Image("map")
+          Text("지도")
+          
+        }
+        .tag(Tab.map)
+      ToiletView()
+        .tabItem {
+          Image("wc")
+          Text("화장실")
+        }
+        .tag(Tab.toilet)
+      ChargerView()
+        .tabItem {
+          Image("battery_charging_full")
+          Text("급속충전기")
+        }
+        .tag(Tab.charger)
+      LiftView()
+        .tabItem {
+          Image("accessible")
+          Text("리프트 위치")
+        }
+        .tag(Tab.lift)
+      ReportView()
+        .tabItem {
+          Image("campaign")
+          Text("민원 등록")
+        }
+        .tag(Tab.report)
     }
+    .accentColor(Color(hex: "874DBC"))
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
