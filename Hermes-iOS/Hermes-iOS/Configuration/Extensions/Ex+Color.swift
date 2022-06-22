fd2303287e8b7492f809215836ee35726a48968d
임주민 <limj1213@gmail.com>
2022-06-22 23:42:16 +0900

    [ADD] Extension 추가

---
+++ Hermes-iOS/Hermes-iOS/Configuration/Extensions/Ex+Color.swift
@@ -0,0 +1,24 @@
+//
+//  Ex+Color.swift
+//  Hermes-iOS
+//
+//  Created by 임주민 on 2022/06/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+  init(hex: String) {
+    let scanner = Scanner(string: hex)
+    _ = scanner.scanString("#")
+    
+    var rgb: UInt64 = 0
+    scanner.scanHexInt64(&rgb)
+    
+    let r = Double((rgb >> 16) & 0xFF) / 255.0
+    let g = Double((rgb >>  8) & 0xFF) / 255.0
+    let b = Double((rgb >>  0) & 0xFF) / 255.0
+    self.init(red: r, green: g, blue: b)
+  }
+}
