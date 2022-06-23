02a00befee4b2ed48a3d1ee0430f242c15a8aeed
임주민 <limj1213@gmail.com>
2022-06-23 14:04:37 +0900

    [FEAT] 필수 입력 항목 별 표시

--- Hermes-iOS/Hermes-iOS/Sources/View/Report/ReportView.swift
+++ Hermes-iOS/Hermes-iOS/Sources/View/Report/ReportView.swift
@@ -20,21 +20,34 @@ struct ReportView: View {
       VStack(spacing: 0){
         CustomNavigationBar()
           .frame(width: 390, height: 100, alignment: .center)
-
+        
         Group {
-          Text("카테고리")
-            .font(.custom("NotoSansKR-Bold", size: 14))
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(EdgeInsets(top: 18, leading: 16, bottom: 5, trailing: 0))
+          HStack(spacing: 0){
+            Text("카테고리")
+              .font(.custom("NotoSansKR-Bold", size: 14))
+              .frame(alignment: .leading)
+              .padding(EdgeInsets(top: 18, leading: 16, bottom: 5, trailing: 0))
+            Text(" *")
+              .font(.custom("NotoSansKR", size: 14))
+              .foregroundColor(Color(hex: "#FF6565"))
+              .frame(maxWidth: .infinity, alignment: .leading)
+              .padding(EdgeInsets(top: 18, leading: 0, bottom: 5, trailing: 0))
+          }
           Rectangle()
             .frame(width: 358, height: 44, alignment: .center)
             .cornerRadius(8)
             .foregroundColor(Color(.white))
-          
-          Text("주소")
-            .font(.custom("NotoSansKR-Bold", size: 14))
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(EdgeInsets(top: 15, leading: 16, bottom: 5, trailing: 0))
+          HStack(spacing: 0){
+            Text("주소")
+              .font(.custom("NotoSansKR-Bold", size: 14))
+              .frame(alignment: .leading)
+              .padding(EdgeInsets(top: 15, leading: 16, bottom: 5, trailing: 0))
+            Text(" *")
+              .font(.custom("NotoSansKR", size: 14))
+              .foregroundColor(Color(hex: "#FF6565"))
+              .frame(maxWidth: .infinity, alignment: .leading)
+              .padding(EdgeInsets(top: 15, leading: 0, bottom: 5, trailing: 0))
+          }
           ZStack{
             TextField("", text: $detailAddress)
               .customTextField(padding: 10)
@@ -74,11 +87,17 @@ struct ReportView: View {
                 .frame(width: 358, height: 188, alignment: .topLeading)
             }
           }
-          
-          Text("첨부파일")
-            .font(.custom("NotoSansKR", size: 14))
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(EdgeInsets(top: 15, leading: 16, bottom: 5, trailing: 0))
+          HStack(spacing: 0){
+            Text("첨부파일")
+              .font(.custom("NotoSansKR", size: 14))
+              .frame(alignment: .leading)
+              .padding(EdgeInsets(top: 15, leading: 16, bottom: 5, trailing: 0))
+            Text(" *")
+              .font(.custom("NotoSansKR", size: 14))
+              .foregroundColor(Color(hex: "#FF6565"))
+              .frame(maxWidth: .infinity, alignment: .leading)
+              .padding(EdgeInsets(top: 15, leading: 0, bottom: 5, trailing: 0))
+          }
         }
         VStack(alignment: .leading) {
           HStack(spacing: 20) {
@@ -101,7 +120,6 @@ struct ReportView: View {
     }
   }
 }
-
 
 struct ReportView_Previews: PreviewProvider {
   static var previews: some View {
