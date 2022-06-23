9a6d20da470259e23321074e92b4a9732c61d086
임주민 <limj1213@gmail.com>
2022-06-23 13:56:38 +0900

    [ADD] Extension 추가

--- Hermes-iOS/Hermes-iOS/Configuration/Extensions/Ex+View.swift
+++ Hermes-iOS/Hermes-iOS/Configuration/Extensions/Ex+View.swift
@@ -8,21 +8,6 @@
 import Foundation
 import SwiftUI
 
-extension View {
-    
-    /// 탭바 숨김 처리 여부
-    /// - Parameter isHidden:
-    /// - Returns:
-  func setTabBarVisibility(isHidden : Bool) -> some View {
-    background(TabBarAccessor { tabBar in
-      // print(">> TabBar height: \(tabBar.bounds.height)")
-      // !! use as needed, in calculations, @State, etc.
-      // 혹은 높이를 변경한다던지 여러가지 설정들이 가능하다.
-      tabBar.isHidden = isHidden
-    })
-  }
-}
-
 // MARK: - UITabBarController 에 TabBar를 가져오기 위한 TabBarAccessor
 struct TabBarAccessor: UIViewControllerRepresentable {
   var callback: (UITabBar) -> Void
@@ -57,3 +42,37 @@ struct TabBarAccessor: UIViewControllerRepresentable {
     }
   }
 }
+
+// MARK: - TextField padding 조정
+struct TextFieldModifier: ViewModifier {
+  let color: Color
+  let padding: CGFloat // <- space between text and border
+  let lineWidth: CGFloat
+  
+  func body(content: Content) -> some View {
+    content
+      .padding(padding)
+      .overlay(RoundedRectangle(cornerRadius: padding)
+                .stroke(color, lineWidth: lineWidth)
+      )
+  }
+}
+
+extension View {
+  /// 탭바 숨김 처리 여부
+  /// - Parameter isHidden:
+  /// - Returns:
+  func setTabBarVisibility(isHidden : Bool) -> some View {
+    background(TabBarAccessor { tabBar in
+      // print(">> TabBar height: \(tabBar.bounds.height)")
+      // !! use as needed, in calculations, @State, etc.
+      // 혹은 높이를 변경한다던지 여러가지 설정들이 가능하다.
+      tabBar.isHidden = isHidden
+    })
+  }
+  
+  func customTextField(color: Color = .secondary, padding: CGFloat = 3, lineWidth: CGFloat = 0.0) -> some View { // <- Default settings
+    self.modifier(TextFieldModifier(color: color, padding: padding, lineWidth: lineWidth))
+  }
+}
+
